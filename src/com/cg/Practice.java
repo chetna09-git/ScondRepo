6333cef1fa65191fd201a12fe92fc804481cdad7
Chetna Gajbhe <chetnagajbhe786@gmail.com>
2021-06-01 17:45:30 +0530

    Git demo added

---
+++ src/com/cg/Practice.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Friends");
+		System.out.println("Welcome");
+		
+
+	}
+
+}
